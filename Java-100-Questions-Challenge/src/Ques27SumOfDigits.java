97612781767bfd945184d98724399a77109acf3a
ParulVerma12345 <parulverma4399@gmail.com>
2026-07-10 21:30:10 +0530

    Question 15 to 27

---
+++ Java-100-Questions-Challenge/src/Ques27SumOfDigits.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Ques27SumOfDigits {
+    public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Welcome to Sum of Digits");
+        System.out.println("Please enter your number:");
+        int num=input.nextInt();
+        int sum=sumOfDigits(num);
+        System.out.println("Sum of Digits is:"+sum);
+    }
+    public static int sumOfDigits(int num){
+        int sum=0;
+        while (num > 0) {
+
+            sum=sum+(num%10);
+            num=num/10;
+        }
+        return sum;
+    }
+}
